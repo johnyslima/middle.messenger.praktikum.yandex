2c4bc6d722832dbd3406619cf1a680be4835bb5b
Мавлютов Эльшат Фуатович <e.mavlyutov@smartdeal.pro>
2023-08-03 20:50:57 +0300

    fix test

--- handlebars.config.js
+++ handlebars.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  partials: "src/Components",
-  helpers: "src/Helpers",
+  partials: 'src/Components',
+  helpers: 'src/Helpers',
 };
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-const express = require("express");
+const express = require('express');
+const history = require('express-history-api-fallback');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(`${__dirname}/../dist`));
+app.use(history('index.html', { root: './dist' }));
 
-app.listen(PORT, function () {
+app.listen(PORT, () => {
+  /* eslint-disable */
   console.log(`Example app listening on port ${PORT}!`);
+  /* eslint-enable */
 });
